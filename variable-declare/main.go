92a77c91ba593fcaaa0cd6d1cb4a92c632ddeacc
Pankaj <pksmks1999@gmail.com>
2025-11-30 21:17:24 +0530

    Lec 1: Variable declare

---
+++ variable-declare/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world from youtube")
+
+	var a int
+	a = 255
+	fmt.Println(a)
+
+	var b string = "Go is awesome!"
+	fmt.Println(b)
+
+	c := 45
+	fmt.Println(c)
+
+	d := "I love programming!"
+	fmt.Println(d)
+}
+
